6e70b8b456c645bd766cdb1836d6964ee4024760
gn0rts <nguyentienmanh2206.2003@gmail.com>
2025-05-16 18:18:35 +0700

    lab07.1

---
+++ Lab07_1/app/src/main/java/com/example/lab07_1/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.lab07_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnOpenChildActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnOpenChildActivity = findViewById(R.id.button);
+        btnOpenChildActivity.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, child_activity.class);
+            startActivity(intent);
+        });
+    }
+
+    private static class child_activity {
+    }
+}
