dd863bd582c0c62789f82863ccf0d6e63a62f1c0
gn0rts <nguyentienmanh2206.2003@gmail.com>
2025-05-16 17:19:49 +0700

    lab04.1

---
+++ Lab04/app/src/main/java/com/example/lab04/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.lab04;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtC, edtF;
+    Button btnCel, btnFar, btnClear;
+    DecimalFormat dcf = new DecimalFormat("#.##");
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtC = findViewById(R.id.edtC);
+        edtF = findViewById(R.id.edtF);
+        btnCel = findViewById(R.id.btnCel);
+        btnFar = findViewById(R.id.btnFar);
+        btnClear = findViewById(R.id.btnClear);
+
+        btnFar.setOnClickListener(v -> {
+            String strF = edtF.getText().toString();
+
+            if (TextUtils.isEmpty(strF)) {
+                Toast.makeText(MainActivity.this, "Please enter Fahrenheit value", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            try {
+                double F = Double.parseDouble(strF);
+                double C = (F - 32) * 5.0 / 9.0;
+                edtC.setText(dcf.format(C));
+            } catch (NumberFormatException e) {
+                Toast.makeText(MainActivity.this, "Invalid Fahrenheit value", Toast.LENGTH_SHORT).show();
+                edtF.setText("");
+            }
+        });
+
+        btnCel.setOnClickListener(v -> {
+            String strC = edtC.getText().toString();
+
+            if (TextUtils.isEmpty(strC)) {
+                Toast.makeText(MainActivity.this, "Please enter Celsius value", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            try {
+                double C = Double.parseDouble(strC);
+                double F = (C * 9.0 / 5.0) + 32;
+                edtF.setText(dcf.format(F));
+            } catch (NumberFormatException e) {
+                Toast.makeText(MainActivity.this, "Invalid Celsius value", Toast.LENGTH_SHORT).show();
+                edtC.setText("");
+            }
+        });
+
+        btnClear.setOnClickListener(v -> {
+            edtC.setText("");
+            edtF.setText("");
+        });
+    }
+}
